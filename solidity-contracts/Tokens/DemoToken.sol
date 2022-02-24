f1e250932bedee76a61f4862f59212a146210a97
Thomas Moran <thomas@optimizme.com>
2022-02-24 20:21:52 +0000

    Demo Token ERC20

--- solidity-contracts/Tokens/DemoToken.sol
+++ solidity-contracts/Tokens/DemoToken.sol
@@ -4,14 +4,16 @@ pragma solidity 0.8 <= 0.9;
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
 
 contract DemoToken is ERC20 {
-    address admin; 
-    constructor() ERC20('Demo Token', 'DTX') {
-        _mint(msg.sender, 10000 * 10 **18);
-    }
+   constructor() ERC20('DemoToken', 'DTX') {
+      mint(msg.sender, 1000 * (10 **18));
+   }
 
-    function mint(address to, uint amount) external {
-        require(msg.sender == admin, 'only admin');
-        _mint(to, amount);
-    }
+   function mint(address recipient, uint amount) private {
+      _mint(recipient, amount);
+   }
+   
+   function faucet(address recipient, uint amount) external {
+      _mint(recipient, amount);
+   }
+   
 }
-
