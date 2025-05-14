04c5586c2cc7ec0224ca3df07eaf702f486b1402
Thomas Moran <thomas@optimizme.com>
2025-05-14 14:41:37 +0100

    update 2025

--- solidity-contracts/Tokens/FlemingToken.sol
+++ solidity-contracts/Tokens/FlemingToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity  >=0.6.0 <0.9.0;
+/* pragma solidity  >=0.6.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 contract FlemingToken is ERC20PresetMinterPauser {
@@ -19,4 +19,22 @@ contract FlemingToken is ERC20PresetMinterPauser {
         super._mint(to, amount);
     }
 
+} */
+
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FlemingToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) 
+        ERC20("FlemingToken", "FLEM") 
+        Ownable(msg.sender) 
+    {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
 }
